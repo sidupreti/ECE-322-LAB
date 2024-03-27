2216e8b619140c2fd3ec4753bbaa8a17022b977e
sidupreti <116125491+sidupreti@users.noreply.github.com>
2024-03-27 15:13:43 -0400

    Update capture_image.c
    Final C code with everything needed

--- Lab2_ECE332_2021/capture_image/capture_image.c
+++ Lab2_ECE332_2021/capture_image/capture_image.c
@@ -1,30 +1,28 @@
-#include "address_map_arm.h"
-#include "sys/alt_timestamp.h"
-#include "sys/alt_stdio.h"
-
+ #include "address_map_arm.h"
+#include <stdio.h>
+#include <time.h>
+#include <string.h>
+//relevant base addresses
 #define KEY_BASE              0xFF200050
 #define VIDEO_IN_BASE         0xFF203060
 #define FPGA_ONCHIP_BASE      0xC8000000
-#define THRESHOLD 7
+#define CHAR_BUFFER_BASE      0xC9000000	// Base address for the character buffer
 
-int counter = 0;
+int counter = 0; // Counter for the num of images processed
+int t; // Threshold
+//declaring functions
+void displayTime(void);
+void clearText(void);
+int getThreshold(short *Video_Mem_ptr);
 
-/* This program demonstrates the use of the D5M camera with the DE1-SoC Board
- * It performs the following: 
- * 	1. Capture one frame of video when any key is pressed.
- * 	2. Display the captured frame when any key is pressed.		  
-*/
-/* Note: Set the switches SW1 and SW2 to high and rest of the switches to low for correct exposure timing while compiling and the loading the program in the Altera Monitor program.
-*/
 int main(void)
-{
-	volatile int * KEY_ptr				= (int *) KEY_BASE;
+{       //estbalishing pointers
+	volatile int * KEY_ptr			= (int *) KEY_BASE; 
 	volatile int * Video_In_DMA_ptr	= (int *) VIDEO_IN_BASE;
 	volatile short * Video_Mem_ptr	= (short *) FPGA_ONCHIP_BASE;
 
 	int x, y;
 
-	alt_printf("Picture count: %d", counter);
 	while (1)
 	{
 		// BLACK AND WHITE IMAGE ============================================================
@@ -40,20 +38,23 @@ int main(void)
 		}
 		
 		counter++;
+		displayTime();
+		t = getThreshold((short *)FPGA_ONCHIP_BASE);
 		for (y = 0; y < 240; y++) {
 			for (x = 0; x < 320; x++) {
 				short colorpixel = *(Video_Mem_ptr + (y << 9) + x);
 				// extracting the red green and blue bits from the rgb from the pixel
+                //applying the correct bit mask
 				int red = (colorpixel >> 11) & 0x1F;
 				int green = (colorpixel >> 5) & 0x3F;
 				int blue = colorpixel & 0x1F;
 
-				//Converting to grayscale, I gave green twice the weight since it has stronger perception to the eye
+				//Converting to grayscale, we gave green twice the weight since it has stronger perception to the eye
 				int grayscale = (red + 2*green + blue) / 4;
 
-				//I applied a static threshold to determine if the pixel is black or white 
+				//Comparing the pixel value to the threshold to detemrine what color the pix should be
 				short bwPixel; 
-				if (grayscale > THRESHOLD) {
+				if (grayscale > t) {
 					bwPixel = 0xFFFF; // White 
 				} else {
 					bwPixel = 0x0000; // Black 
@@ -62,7 +63,6 @@ int main(void)
 				*(Video_Mem_ptr + (y << 9) + x) = bwPixel;
 			}
 		}
-		alt_printf("Picture count: %d", counter);
 		
 		while (1)
 		{
@@ -73,6 +73,7 @@ int main(void)
 		}
 		
 		// RESUME VIDEO =============================================================
+		clearText();
 		*(Video_In_DMA_ptr + 3)	= 0x4;				// Enable the video
 		while (1)
 		{
@@ -95,12 +96,15 @@ int main(void)
 				break;
 			}
 		}
-		
+		//same process as b to white but now switching the colors
 		counter++;
+		displayTime();
+		t = getThreshold((short *)FPGA_ONCHIP_BASE); 
 		for (y = 0; y < 240; y++) {
 			for (x = 0; x < 320; x++) {
 				short colorpixel = *(Video_Mem_ptr + (y << 9) + x);
 				// extracting the red green and blue bits from the rgb from the pixel
+                //applying the correct bit mask
 				int red = (colorpixel >> 11) & 0x1F;
 				int green = (colorpixel >> 5) & 0x3F;
 				int blue = colorpixel & 0x1F;
@@ -110,7 +114,7 @@ int main(void)
 
 				//I applied a static threshold to determine if the pixel is black or white 
 				short bwPixel; 
-				if (grayscale > THRESHOLD) {
+				if (grayscale > t) {
 					bwPixel = 0x0000; // White 
 				} else {
 					bwPixel = 0xFFFF; // Black 
@@ -119,7 +123,6 @@ int main(void)
 				*(Video_Mem_ptr + (y << 9) + x) = bwPixel;
 			}
 		}
-		alt_printf("Picture count: %d", counter);
 		
 		while (1)
 		{
@@ -130,6 +133,7 @@ int main(void)
 		}
 		
 		// RESUME VIDEO =============================================================
+		clearText();
 		*(Video_In_DMA_ptr + 3)	= 0x4;				// Enable the video
 		while (1)
 		{
@@ -142,6 +146,7 @@ int main(void)
 		}
 		
 		// VERTICALLY FLIPPED IMAGE ===============================================================
+        //only looking at the first half of the image and swapping it with the bottom half, hence the 120
 		*(Video_In_DMA_ptr + 3)	= 0x4;				// Enable the video
 		while (1)
 		{
@@ -154,6 +159,7 @@ int main(void)
 		}
 		
 		counter++;
+		displayTime();
 		for (y = 0; y < 120; y++) {
 			for (x = 0; x < 320; x++) {
 				int mirrorY = 239 - y;
@@ -162,7 +168,6 @@ int main(void)
 				*(Video_Mem_ptr + (mirrorY << 9) + x) = temp;
 			}
 		}
-		alt_printf("Picture count: %d", counter);
 		
 		while (1)
 		{
@@ -173,6 +178,7 @@ int main(void)
 		}
 		
 		// RESUME VIDEO =============================================================
+		clearText();
 		*(Video_In_DMA_ptr + 3)	= 0x4;				// Enable the video
 		while (1)
 		{
@@ -185,6 +191,7 @@ int main(void)
 		}
 		
 		// HORIZONTALLY FLIPPED IMAGE ===============================================================
+        //only looking at the left half of the image and swapping it with the right half, hence the 160
 		*(Video_In_DMA_ptr + 3)	= 0x4;				// Enable the video
 		while (1)
 		{
@@ -197,6 +204,7 @@ int main(void)
 		}
 		
 		counter++;
+		displayTime();
 		for (y = 0; y < 240; y++) {
 			for (x = 0; x < 160; x++) {
 				int mirrorX = 319 - x;
@@ -205,7 +213,6 @@ int main(void)
 				*(Video_Mem_ptr + (y << 9) + mirrorX) = temp;
 			}
 		}
-		alt_printf("Picture count: %d", counter);
 		
 		while (1)
 		{
@@ -216,6 +223,7 @@ int main(void)
 		}
 		
 		// RESUME VIDEO =============================================================
+		clearText();
 		*(Video_In_DMA_ptr + 3)	= 0x4;				// Enable the video
 		while (1)
 		{
@@ -227,4 +235,69 @@ int main(void)
 			}
 		}
 	}
+}
+
+// Modified getCurrentTime to display time on the VGA screen using the character buffer
+void displayTime(void) {
+	// Get current time
+	struct tm* timeinfo;
+	time_t rawtime = time(NULL);
+	timeinfo = localtime( &rawtime );
+	
+	// Print timestamp
+	int x = 1;
+	int y = 1;
+	int offset;
+	char *text = asctime(timeinfo);
+
+	offset = (y << 7) + x; // Calculate the offset based on the x, y position
+	while(*text && *text != '\n') {
+		*((volatile char*)(CHAR_BUFFER_BASE + offset)) = *(text);
+		++text;
+		++offset;
+	}
+	
+	// Print image count
+	y = 2;
+	char counter_str[10];
+	sprintf(counter_str, "%d", counter);
+	
+	offset = (y << 7) + x;
+	char *count = counter_str;
+	while(*count) {
+		*((volatile char*)(CHAR_BUFFER_BASE + offset)) = *(count);
+		++count;
+		++offset;
+	}
+}
+
+// Fill the char buffer with spaces
+void clearText(void) {
+    volatile char *char_buffer = (volatile char *)CHAR_BUFFER_BASE;
+    int i;
+
+    // Iterate through the char buffer and set each character to a space
+    for (i = 0; i < 80 * 60; ++i) {
+        char_buffer[i] = ' ';
+    }
+}
+
+// Get average grayscale value for black/white threshold
+int getThreshold(short *Video_Mem_ptr) {
+	int x, y;
+	int sum = 0;
+	for (y = 0; y < 240; y++) {
+		for (x = 0; x < 320; x++) {
+			short colorpixel = *(Video_Mem_ptr + (y << 9) + x);
+			// extracting the red green and blue bits from the rgb from the pixel
+			int red = (colorpixel >> 11) & 0x1F;
+			int green = (colorpixel >> 5) & 0x3F;
+			int blue = colorpixel & 0x1F;
+
+			//Converting to grayscale, I gave green twice the weight since it has stronger perception to the eye
+			int grayscale = (red + 2*green + blue) / 4;
+			sum += grayscale;
+		}
+	}
+	return sum/(240*320);
 }
